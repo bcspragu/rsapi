8b2a37c50510dddd7c76b53681ff8db20e5cc87e
Brandon Sprague <brandon@sprague.mx>
2023-11-24 13:27:47 -0800

    chore(release): publish

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -11,3 +11,13 @@ do
     (cd $dir && npm publish --access public)
   fi
 done
+
+DIRS=$(find ./packages/rsapi/npm -type d -maxdepth 1 | sort -r)
+
+for dir in $DIRS
+do
+  if [ -f "$dir/package.json" ]; then
+    echo "Publishing $dir"
+    (cd $dir && npm publish --access public)
+  fi
+done
